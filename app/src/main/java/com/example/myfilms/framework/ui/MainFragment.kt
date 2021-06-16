8ddcc047aa68092bc01908b70ed7c180ab192fb7
Ekaterina <yeldog@mail.ru>
2021-06-16 18:04:24 +0300

    liveData subscribed

--- app/src/main/java/com/example/myfilms/framework/ui/MainFragment.kt
+++ app/src/main/java/com/example/myfilms/framework/ui/MainFragment.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import com.example.myfilms.R
 import com.example.myfilms.databinding.MainFragmentBinding
+
 
 class MainFragment : Fragment() {
 
@@ -23,7 +26,13 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        val observer = Observer <Any> {renderData (it)} //выполняет renderData сразу, как только LiveData обновляет данные
+        viewModel.getData().observe(viewLifecycleOwner, observer) ////viewLifecycleOwner - универсальня ссылка на активити или фрагмент
+    }
+
+    private fun renderData(data: Any?) {
+        Toast.makeText(context, "data", Toast.LENGTH_LONG).show()
     }
 
     companion object {
--- app/src/main/java/com/example/myfilms/framework/ui/MainViewModel.kt
+++ app/src/main/java/com/example/myfilms/framework/ui/MainViewModel.kt
@@ -1,7 +1,26 @@
 package com.example.myfilms.framework.ui
 
+import android.os.SystemClock.sleep
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel (private val liveDataToObserve : MutableLiveData<Any> =
+        MutableLiveData()) :
+        ViewModel() {
+
+            fun getData() : LiveData<Any> {
+                getDataFromLocalSource()
+                return liveDataToObserve
+                        //возвращает LiveData всем, кто хочет подписаться на изменения данных
+            }
+// //liveData - оберточный класс над данными. MutableLiveData можно изменять, на LiveData можно только подписаться
+
+    private fun getDataFromLocalSource() {
+        Thread {
+            sleep(1000)
+            liveDataToObserve.postValue(Any()) //postValue - обновление данных из рабочего потока, setValue из основного
+        }.start()
+    }
+
 }
