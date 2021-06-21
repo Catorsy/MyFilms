ace1877ce5b808d55bfddb703c20b7dacfee5ab3
Ekaterina <yeldog@mail.ru>
2021-06-21 22:03:40 +0300

    adapterOn

--- app/src/main/java/com/example/myfilms/view/MainFragment.kt
+++ app/src/main/java/com/example/myfilms/view/MainFragment.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myfilms.R
+import com.example.myfilms.adapter.MainFragmentAdapter
 import com.example.myfilms.databinding.MainFragmentBinding
 import com.example.myfilms.viewModel.MainViewModel
 import com.example.myfilms.viewModel.AppState
@@ -22,6 +23,7 @@ class MainFragment : Fragment() {
 
     private lateinit var binding: MainFragmentBinding
     private val viewModel: MainViewModel by viewModel()
+                private val adapter = MainFragmentAdapter() //не было
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
@@ -32,7 +34,10 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
        // viewModel = ViewModelProvider(this).get(MainViewModel::class.java) //не через koin
-        lifecycle.addObserver(viewModel)
+                //lifecycle.addObserver(viewModel)
+
+                    binding.recyclerView.adapter = adapter
+
         val observer = Observer <AppState> {renderData (it)} //выполняет renderData сразу, как только LiveData обновляет данные
         viewModel.getLiveData().observe(viewLifecycleOwner, observer) ////viewLifecycleOwner - универсальня ссылка на активити или фрагмент
         viewModel.getMoviesFromLocalSource()
@@ -43,10 +48,11 @@ class MainFragment : Fragment() {
         //сюда адаптер TODO
         when (appState) {
             is AppState.Success -> {
-                val moviesData = appState.moviesData
+                //val moviesData = appState.moviesData //для не-списка
+               // setData(moviesData)
                 waitForIt.visibility = View.GONE
-                setData(moviesData)
-                Snackbar.make(recyclerView, "Success", Snackbar.LENGTH_LONG).show()
+                adapter.setListOfMovies(appState.moviesData)
+                Snackbar.make(recyclerView, getString(R.string.sucess), Snackbar.LENGTH_LONG).show()
             }
             is AppState.Loading -> {
                 waitForIt.visibility = View.VISIBLE
@@ -61,18 +67,18 @@ class MainFragment : Fragment() {
 
     }
 
-            //"образец"... данные
-    private fun setData(moviesData: Movies) {
-        binding.myFavorTitle.text = moviesData.name
-        binding.myFavorRange.text = moviesData.vote_average.toString()
-                binding.testButton.setOnClickListener {
-                    activity?.supportFragmentManager?.beginTransaction()
-                        ?.add(R.id.container, MovieItemFragment.newInstance(moviesData))?.addToBackStack(null)
-                        ?.commit()
-                    Snackbar.make(binding.recyclerView, "Переход во фрагмент", Snackbar.LENGTH_LONG).show()
-                }
-
-    }
+            //старый метод не для списка
+//    private fun setData(moviesData: Movies) {
+//        binding.myFavorTitle.text = moviesData.name
+//        binding.myFavorRange.text = moviesData.vote_average.toString()
+//                binding.testButton.setOnClickListener {
+//                    activity?.supportFragmentManager?.beginTransaction()
+//                        ?.add(R.id.container, MovieItemFragment.newInstance(moviesData))?.addToBackStack(null)
+//                        ?.commit()
+//                    Snackbar.make(binding.recyclerView, "Переход во фрагмент", Snackbar.LENGTH_LONG).show()
+//                }
+//
+//    }
 
     companion object {
         fun newInstance() = MainFragment()
