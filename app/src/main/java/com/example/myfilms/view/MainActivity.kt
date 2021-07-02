c5e75782fca940b6d12ed2d14ab2e60cb034193e
Ekaterina <yeldog@mail.ru>
2021-07-02 19:55:35 +0300

    please2

--- app/src/main/java/com/example/myfilms/view/MainActivity.kt
+++ app/src/main/java/com/example/myfilms/view/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myfilms.R
 
+//Я заглушка! Дз изготавливается, подождите, пожалуйста, если можно :)
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
